1cee53daee0fb5d08503e4f1e5e003d859b6546f
Sem8 <mssemmi8@gmail.com>
2019-05-30 18:16:05 -0400

    model for firebase oauth sign up login written

--- models/users.js
+++ models/users.js
@@ -6,8 +6,24 @@ const db = require('../database/dbconfig');
       "firebase_id",
       "email",
     );
+  };
+
+  async function registerOrLogin(user) {
+    try {
+      const loggedInUser = await db('users').where({ firebase_id: user.firebase_id}).first();
+      if(!!loggedInUser) {
+        return loggedInUser;
+      } else {
+        const [id] = await db('users').insert(user, 'id');
+        const signedUpUser = await db('users').where({ id: id }).first();
+        return signedUpUser;
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   module.exports = {
-      find
+      find,
+      registerOrLogin
   }
