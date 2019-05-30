61c724030bcea16246921895303a0f143f704a38
Latifah President <latifahpresident@Latifahs-MacBook-Pro.local>
2019-05-30 15:28:37 -0500

    added the vendors table

--- database/migrations/20190530151947_market.js
+++ database/migrations/20190530151947_market.js
@@ -2,7 +2,14 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('market', market => {
         market.increments().primary()
-        market.string('firebase_id').notNullable().unique()
+        market
+            .string("firebase_id")
+            .unsigned()
+            .unique()
+            .notNullable()
+            .references("firebase_id")
+            .inTable("users")
+            .onUpdate("CASCADE");
         market.string('name')
         market.integer('avail_stalls')
     })
---
+++ database/migrations/20190530152250_vendor.js
@@ -0,0 +1,32 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('vendor', vendor => {
+        vendor.increments()
+        vendor
+            .string("firebase_id")
+            .unsigned()
+            .unique()
+            .notNullable()
+            .references("firebase_id")
+            .inTable("users")
+            .onUpdate("CASCADE");
+        vendor.string('company_name')
+        vendor.string('contact_fullname')
+        vendor.string('address')
+        vendor.string('city')
+        vendor.string('state')
+        vendor.string('zip_code')
+        vendor.string('phone')
+        vendor.string('company_url')
+        vendor
+          .integer('market_id')
+          .unsigned()
+          .notNullable()
+          .references('id')
+          .inTable('market')
+    })
+  };
+  
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('vendor')
+  };
