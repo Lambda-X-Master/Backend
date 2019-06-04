12ad2d52425f060d26c40c4e3a3235cf807cdce9
Sem8 <mssemmi8@gmail.com>
2019-06-04 14:18:26 -0400

    routes users changes

--- routes/users.js
+++ routes/users.js
@@ -5,9 +5,10 @@ const usersController = require('../controllers/users');
 const router = express.Router();
 const isAuthenticated = require("../middleware/firebase.js");
 
-router.get('/', usersController.findUsers)
+
 router.post("/register", isAuthenticated, usersController.registerOrLogin); 
 router.get("/login", usersController.registerOrLogin);
+router.get('/', isAuthenticated, usersController.findUsers);
 
 // router.post("/register", isAuthenticated, usersController.registerOrLoginFB);
 // router.get('/login/:id', usersController.login);
