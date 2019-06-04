61415a54cafbde8980cf86f26c92d9ce14eb3fbf
Francis Chen <fncis.chen@gmail.com>
2019-06-04 10:55:11 -0700

    updated market, vendor, stall, and car migrations

--- database/migrations/20190530153550_stall.js
+++ database/migrations/20190530153550_stall.js
@@ -1,15 +1,16 @@
 
     exports.up = function(knex, Promise) {
         return knex.schema.createTable('stall', stall => {
-            stall.increments()
-            stall.string('name')
-            stall.string('size')
+            stall.increments();
+            stall.json('size')
             stall
               .integer('market_id')
               .unsigned()
               .notNullable()
               .references('id')
               .inTable('market')
+            stall.boolean("available");
+            stall.integer("qty");
         })
       };
 
