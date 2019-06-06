4cc25a60cef2b4254192e1f94995354d5a0f5060
Latifah President <latifahpresident@Latifahs-MacBook-Pro.local>
2019-06-06 11:29:28 -0500

    added console log to get markets

--- controllers/market.js
+++ controllers/market.js
@@ -6,5 +6,6 @@ exports.getAllMarkets = async (req, res, next) => {
         res.status(200).json(markets)
     } catch (err) {
         res.status(500).json(`There was an error getting all markets`)
+        console.log(err, 'error from get all markets')
     }
 }
--- controllers/users.js
+++ controllers/users.js
@@ -27,10 +27,10 @@ exports.findUsers = async (req, res, next) => {
 exports.registerOrLogin = async (req, res, next) => {
   try {
     // console.log("Test");
-    const { email } = req.body;
-    // console.log("Our cool request body: ", req.body);
+    const { email, user_type } = req.body;
+    console.log("Our cool request body: ", req.body);
     const firebase_id = req.user
-    const registerToUser = await Users.registerOrLogin({ firebase_id, email });
+    const registerToUser = await Users.registerOrLogin({ firebase_id, email, user_type });
     res.status(201).json(registerToUser);
   } catch (error) {
     console.log(error);
