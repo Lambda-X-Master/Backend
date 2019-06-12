0c28f4a6e713e8c675b5c8643157521853820101
Sem8 <mssemmi8@gmail.com>
2019-06-12 12:09:16 -0400

    took out isAuthenticated middleware from product delete route to troubleshoot product delete on frontend

--- routes/product.js
+++ routes/product.js
@@ -5,9 +5,9 @@ const productsController = require('../controllers/product');
 const router = express.Router();
 const isAuthenticated = require("../middleware/firebase.js");
 
-router.get("/vendor/:vendor_id", productsController.getProductsByVendorId)
+router.get("/vendor/:vendor_id", productsController.getProductsByVendorId);
 router.post("/vendor/:vendor_id", isAuthenticated, productsController.addProductByVendorId); 
 router.put("/:product_id", isAuthenticated, productsController.updateProductByProductId);
-router.delete("/:product_id", isAuthenticated, productsController.deleteProductByProductId);
+router.delete("/:product_id", productsController.deleteProductByProductId);
 
 module.exports = router; 
