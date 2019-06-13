de7bd094c034c3c4c8e712aa7101be2f278d4c88
Francis Chen <fncis.chen@gmail.com>
2019-06-13 13:59:38 -0700

    changed case of the cart and market controllers

--- controllers/cart.js
+++ controllers/cart.js
@@ -1,10 +1,10 @@
-const cart = require('../models/cart');
+const Cart = require('../models/cart');
 
 exports.getVendorCart = async (req, res, next) => {
     try {
         // const id = req.params.id
         console.log(typeof id, 'get cart id')
-        const vendorCart = await cart.getCartById(req.params.id)
+        const vendorCart = await Cart.getCartById(req.params.id)
         res.status(200).json(vendorCart)
     } catch (err) {
         res.status(500).json({message: `error getting vendors cart`})
@@ -15,7 +15,7 @@ exports.getVendorCart = async (req, res, next) => {
 exports.getCarts = async (req, res, next) => {
     try {
         const id = req.params.id
-        cartData = await cart.getCart()
+        cartData = await Cart.getCart()
         console.log(req.body)
         res.status(200).json(cartData)
         console.log(cartData, 'cart created')
@@ -61,7 +61,7 @@ exports.addStallToCart = async (req, res, next) => {
         let stalls_id = req.body.stalls_id;
         // console.log(req, "req");
         console.log(req.body, 'stall fron at to cart')
-        const addedStall = await cart.addStallToCart(stalls_id, cart_id)
+        const addedStall = await Cart.addStallToCart(stalls_id, cart_id)
         res.status(201).json(addedStall)
     } catch (err) {
         res.status(500).json(`error adding cart`)
@@ -75,7 +75,7 @@ exports.removeStallFromCart = async (req, res, next) => {
         let stalls_id = req.body.stalls_id;
         // console.log(req, "req");
         console.log(req.body, 'deletedStall')
-        const removedStall = await cart.removeStallFromCart(stalls_id, cart_id)
+        const removedStall = await Cart.removeStallFromCart(stalls_id, cart_id)
         res.status(201).json(removedStall)
         } catch (err) {
             res.status(500).json(`error removing cart`)
