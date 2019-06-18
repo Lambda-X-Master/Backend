7f1a83611ae7375a20691127e3f565d5828bbdf8
Francis Chen <fncis.chen@gmail.com>
2019-06-18 10:29:52 -0700

    removed market_id from orders table

--- database/migrations/20190617143725_orders.js
+++ database/migrations/20190617143725_orders.js
@@ -8,19 +8,11 @@ exports.up = function(knex, Promise) {
         .notNullable()
         .references('firebase_id')
         .inTable('vendor')
-        .unique();
-        orders.int("stall_id")
+        orders.integer("stall_id")
         .unsigned()
         .notNullable()
         .references("id")
         .inTable("stall")
-        .unique();
-        orders.string('market_id')
-        .unsigned()
-        .notNullable()
-        .references('market_id')
-        .inTable('stall')
-        .unique();
         orders.json('size');
         orders.float('price');
     })
